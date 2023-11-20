4aca1168d771eeddfb83c71d8a228cd14642d4d3
katesem <katewhite922@gmail.com>
2023-11-20 22:27:37 +0200

    add KafkaProduces class for user service

---
+++ user-service/src/main/java/com/user/kafka/KafkaProducer.java
@@ -0,0 +1,23 @@
+package com.user.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private static final String TOPIC = "user-topic";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void produceMessage(String message) {
+        System.out.println("Sending message to user-topic: " + message);
+        kafkaTemplate.send(TOPIC, message);
+    }
+}
